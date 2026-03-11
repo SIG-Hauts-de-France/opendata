3b4e75a7cf0e0b950d84a5674c52293a4d5e761c
Matthieu Etourneau <metourneau@neogeo.fr>
2026-03-11 16:02:44 +0100

    feat: add matomo

--- apps/datahub/src/main.ts
+++ apps/datahub/src/main.ts
@@ -24,6 +24,7 @@ loadAppConfig().then(() => {
   matomoScript.type = 'text/javascript'
   matomoScript.text = `
     var _paq = window._paq = window._paq || [];
+    _paq.push(['alwaysUseSendBeacon', true]);
     _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
     (function() {
@@ -33,6 +34,12 @@ loadAppConfig().then(() => {
       var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
       g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
     })();
+    document.addEventListener('click', function(e) {
+      var link = e.target.closest('a[href$=".pdf"],a[href$=".zip"],a[href$=".doc"],a[href$=".docx"],a[href$=".xls"],a[href$=".xlsx"],a[href$=".csv"]');
+      if (link) {
+        _paq.push(['trackLink', link.href, 'download']);
+      }
+    });
   `
   document.head.appendChild(matomoScript)
 })
